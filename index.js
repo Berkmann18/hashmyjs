294ac8892c10903c627e3fd2df7428f6247da38a
Berkmann18 <maxieberkmann@gmail.com>
2018-08-09 21:04:33 +0100

    Attempted at fixing #13

--- index.js
+++ index.js
@@ -14,16 +14,23 @@ const hmj = require('./src/hashmyjs.js'),
 
 clr.setTheme(require('./src/clr'));
 
+const str = (val) => val;
+
+
 prgm.arguments('[files...]')
   .version(pkg.version)
   .description(pkg.description)
-  .option('-f, --format [format]', 'Specify the format of the output (text (default), json, csv)', String)
-  .option('-o, --output [path]', 'Output to a file instead of in the STDOUT', String)
+  .option('-f, --format [format]', 'Specify the format of the output (text (default), json, csv)', str, 'text')
+  .option('-o, --output [path]', 'Output to a file instead of in the STDOUT', str, 'stdout')
   .option('-i, --interactive', 'Forces to read the input from the STDIN')
   .option('-p, --prettify', 'Prettify the output')
   .action((files) => {
+    // console.debug('format=', prgm.format);
+    // console.debug('output=', prgm.output);
+    // console.debug('interactive=', prgm.interactive);
+
     validInput = true;
     let input = (prgm.interactive) ? 'stdin' : 'any';
-    hmj.run(prgm.format || 'text', input, prgm.output || 'STDOUT', files, !!prgm.prettify);
+    hmj.run(prgm.format, input, prgm.output, files, !!prgm.prettify);
   })
   .parse(process.argv);
