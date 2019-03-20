ac28b3099fd448a1cb8a221c0dff9b7ccb1157f6
Berkmann18 <maxieberkmann@gmail.com>
2019-03-20 17:20:33 +0000

    Update: readFiles is now promise-based
    Most functions (namely `readFilesSync`) are now promise-based and thus
    returning a different output.

--- src/files.js
+++ src/files.js
@@ -10,11 +10,12 @@ const fs = require('fs'),
 const { OUTPUT_DEST, OUTPUT_FORMAT, scanInput, csvHandler, jsonHandler, writeToFile } = require('./core');
 
 /**
+ * @todo REMOVE "Sync" (Breaking change?)
  * @description Synchronously read files and scan them.
  * @param {string[]} [files] Array of file paths
  * @param {Config} obj Configuration
  * @see Config
- * @return {(undefined|string[]|{...string})} Data or nothing
+ * @return {Promise<(undefined|string[]|{...string})>} Data or nothing
  * @public
  * @example <caption>Reading from the CLI</caption>
  * readFilesSync();
@@ -35,18 +36,21 @@ const readFilesSync = (
   let inputs = [],
     res = {};
 
-  for (let i = 0, len = files.length; i < len; ++i) {
-    try {
-      inputs.push(fs.readFileSync(files[i]));
-    } catch (err) {
-      error(`File "${files[i]}" Not Found!`);
-      return err.message;
+  return new Promise((resolve, reject) => {
+    for (let i = 0, len = files.length; i < len; ++i) {
+      try {
+        inputs.push(fs.readFileSync(files[i]));
+      } catch (err) {
+        error(`File "${files[i]}" Not Found!`);
+        reject(err); //err.message
+      }
+
+      res[files[i]] = scanInput(inputs[i], true);
     }
 
-    res[files[i]] = scanInput(inputs[i], true);
-  }
+    resolve(processFileData(res, { prettify, outputDest, outputFormat }));
+  });
 
-  return processFileData(res, { prettify, outputDest, outputFormat });
 };
 
 /**
@@ -54,7 +58,7 @@ const readFilesSync = (
  * @param {{string: string}} data Data to process
  * @param {{string, boolean}} [config={outputDest=OUTPUT_DEST, prettify=false}] Configuration with output destination
  * and whether or not it prettifies the output
- * @returns {undefined|string|Object|Array} Processed JSON result
+ * @returns {boolean|(string|Object|Array)|Promise} Processed JSON result
  */
 const processJsonData = (data, { outputDest = OUTPUT_DEST, prettify = false } = {}) => {
   switch (outputDest) {
@@ -72,26 +76,30 @@ const processJsonData = (data, { outputDest = OUTPUT_DEST, prettify = false } = 
  * @param {{string: string}} data Data read from the files
  * @param {Config} obj Configuration
  * @see Config
- * @returns {undefined|string|Object|Array} Processed result
+ * @returns {Promise<undefined|string|Object|Array>} Processed result
  */
 const processFileData = (data, { prettify = false, outputDest = OUTPUT_DEST, outputFormat = OUTPUT_FORMAT } = {}) => {
-  if (outputFormat === 'json') return processJsonData(data, { outputDest, prettify });
-
-  let result = [];
-  const TO_STDOUT = outputDest === 'stdout';
-
-  if (outputFormat === 'csv') {
-    for (let file in data) {
-      let csv = csvHandler(file, data[file], prettify);
-      TO_STDOUT ? out(csv) : result.push(csv);
+  return new Promise((resolve) => {
+    if (outputFormat === 'json') {
+      resolve(processJsonData(data, { outputDest, prettify }));
+      return; //Only here because it doesn't stop here
     }
-  } else {
-    for (let file in data) {
-      TO_STDOUT ? out(`- ${file}: ${data[file]}`) : result.push(data[file]);
+    let result = [];
+    const TO_STDOUT = outputDest === 'stdout';
+
+    if (outputFormat === 'csv') {
+      for (let file in data) {
+        let csv = csvHandler(file, data[file], prettify);
+        TO_STDOUT ? out(csv) : result.push(csv);
+      }
+    } else {
+      for (let file in data) {
+        TO_STDOUT ? out(`- ${file}: ${data[file]}`) : result.push(data[file]);
+      }
     }
-  }
-  if (TO_STDOUT) return;
-  return (outputDest === 'var') ? result : writeToFile(outputDest, result);
+    if (TO_STDOUT) resolve();
+    resolve((outputDest === 'var') ? result : writeToFile(outputDest, result));
+  });
 };
 
 module.exports = readFilesSync;
--- test/files.test.js
+++ test/files.test.js
@@ -2,7 +2,8 @@ const readFilesSync = require('../src/files');
 const stdout = require('test-console').stdout;
 
 test('Not existent', () => {
-  expect(readFilesSync(['nowt'])).toEqual('ENOENT: no such file or directory, open \'nowt\'');
+  expect(readFilesSync(['nowt']))
+    .rejects.toThrowError('ENOENT: no such file or directory, open \'nowt\'');
 });
 
 let ex0 = './examples/ex0.js', csv = './test/gen/out.csv', jso = './test/gen/out.json',
@@ -13,33 +14,41 @@ let ex0 = './examples/ex0.js', csv = './test/gen/out.csv', jso = './test/gen/out
   strJson0 = JSON.stringify(json0);
 
 test(`readFilesSync(["${ex0}"], {outputFormat=csv, outputDest="${csv}"})`, () => {
-  expect(readFilesSync([ex0], { outputFormat: 'csv', outputDest: csv })).toBeUndefined();
+  expect(readFilesSync([ex0], { outputFormat: 'csv', outputDest: csv })).resolves.toBeUndefined();
 });
 
 test(`readFilesSync(["${ex0}"], {outputFormat=json, outputDest="${jso}"})`, () => {
-  expect(readFilesSync([ex0], { outputFormat: 'json', outputDest: jso })).toBeUndefined();
+  expect(readFilesSync([ex0], { outputFormat: 'json', outputDest: jso })).resolves.toBeUndefined();
 });
 
 test(`readFileSync(files=["${ex0}"], {outputFormat=text, outputDest=var})`, () => {
-  expect(readFilesSync([ex0], { outputFormat: 'text', outputDest: 'var' })).toEqual([hashEx0])
+  expect(readFilesSync([ex0], { outputFormat: 'text', outputDest: 'var' })).resolves.toEqual([hashEx0])
 });
 
 test(`readFileSync(files=["${ex0}"], {outputFormat=json, outputDest=var})`, () => {
-  expect(readFilesSync([ex0], { outputFormat: 'json', outputDest: 'var' })).toEqual(json0)
+  expect(readFilesSync([ex0], { outputFormat: 'json', outputDest: 'var' })).resolves.toEqual(json0)
 });
 
 test(`readFileSync(files=["${ex0}"], {outputFormat=csv, outputDest=var})`, () => {
-  expect(readFilesSync([ex0], { outputFormat: 'csv', outputDest: 'var' })).toEqual([csv0])
+  expect(readFilesSync([ex0], { outputFormat: 'csv', outputDest: 'var' })).resolves.toEqual([csv0])
 });
 
 const OUT_START = '\u001b[1m\u001b[36m',
   OUT_END = '\u001b[39m\u001b[22m\n';
 
 test(`readFilesSync(files=["${ex0}"], {outputFormat=json, outputDest=stdout})`, () => {
-  const output = stdout.inspectSync(() => readFilesSync([ex0], { outputFormat: 'json', outputDest: 'stdout' }));
-  expect(output).toEqual([`${OUT_START}${strJson0}${OUT_END}`])
+  const inspect = stdout.inspect();
+  expect.assertions(2);
+  readFilesSync([ex0], { outputFormat: 'json', outputDest: 'stdout' })
+    .then(out => {
+      inspect.restore();
+      expect(out).toBeUndefined();
+      expect(inspect.output).toEqual([`${OUT_START}${strJson0}${OUT_END}`]);
+    }, err => {
+      throw new Error(err);
+    });
 });
 
 test('readFilesSync([], {outputFormat=csv, outputDest="nothing"})', () => {
-  expect(readFilesSync([], { outputFormat: 'json', outputDest: './test/gen/nothing' })).toBeUndefined();
+  expect(readFilesSync([], { outputFormat: 'json', outputDest: './test/gen/nothing' })).resolves.toBeUndefined();
 });
